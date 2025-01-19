15eea06ca1f0ed145add16e898630356fdeceea6
Muha11ad <abdushukurovmukhammadjon70@gmail.com>
2025-01-19 11:52:34 +0500

    fix (hope) : redis

--- src/common/configs/redis.config.ts
+++ src/common/configs/redis.config.ts
@@ -5,8 +5,6 @@ import { CacheModuleAsyncOptions } from '@nestjs/cache-manager';
 export const redisOptions: CacheModuleAsyncOptions = {
   imports: [ConfigModule],
   useFactory: async (configService: ConfigService) => {
-    const password = configService.get<string>('REDIS_PASSWORD')
-    console.log(password)
     try {
       const store = await redisStore({
         socket: {
@@ -14,9 +12,7 @@ export const redisOptions: CacheModuleAsyncOptions = {
           port: parseInt(configService.get<string>('REDIS_PORT'), 10),
           reconnectStrategy: (retries) => Math.min(retries * 50, 2000),
           connectTimeout: 10000,
-          
         },
-        password
       });
 
       return {
