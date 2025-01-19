d358a4e73c42e343f30cd151685004af009549a4
Muha11ad <abdushukurovmukhammadjon70@gmail.com>
2025-01-19 12:19:20 +0500

    fix : serve static file

--- src/common/common-service.module.ts
+++ src/common/common-service.module.ts
@@ -1,20 +1,14 @@
-import { pathToUpload } from './utils';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CacheModule } from '@nestjs/cache-manager';
 import { mailOptions, redisOptions } from './configs';
 import { MailerModule } from '@nestjs-modules/mailer';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { DatabaseService, EmailService, FilesService, RedisService } from './services';
 @Module({
   imports: [
     ConfigModule,
     MailerModule.forRootAsync(mailOptions),
     CacheModule.registerAsync(redisOptions),
-    ServeStaticModule.forRoot({
-      rootPath: pathToUpload,
-      renderPath: '/uploads',
-    }),
   ],
   providers: [EmailService, DatabaseService, FilesService, RedisService],
   exports: [EmailService, DatabaseService, FilesService, RedisService, CacheModule],
--- src/common/utils/paths.ts
+++ src/common/utils/paths.ts
@@ -1,4 +1,4 @@
 import * as path from 'path';
 
-export const pathToSave = path.resolve(__dirname, '../../../../uploads');
-export const pathToUpload = path.resolve(__dirname, '../../../../');
+export const pathToSave = path.resolve(__dirname, '../../../../../uploads');
+export const pathToUpload = path.resolve(__dirname, '../../../../../');
